13fde4f2453caa779057749f4b43e69e0c522b92
Animesh Arya <arya07@Animeshs-MacBook-Air.local>
2025-01-29 14:05:18 +0530

    Pig Game v-0

---
+++ Pig Game/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// starting condition
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currScore = 0;
+
+// Rolling dice functionality
+
+btnRoll.addEventListener('click', function () {
+  //1. Generating a randon dice Roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //3. Check for rolled 1: if true,
+  if (dice !== 1) {
+    // Add dice to current Score
+    currScore += dice;
+    current0El.textContent = currScore;
+  } else {
+    // switch to next player
+
+  }
+});
